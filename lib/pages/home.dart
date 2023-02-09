6dc155ffee8ed8eccc4df78098b7e16c56d8a404
Rick <ricardohs@ricardohs.com>
2023-02-09 15:33:21 -0600

    Search History working

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/model/movie.dart';
 import 'package:movie_app/pages/movie_details.dart';
+import 'package:localstore/localstore.dart';
+import 'package:movie_app/widgets/search_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,24 +17,63 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    final searchBar = Column(
-      children: [
-        const Text(
-          "Place the name of the movie you want to know its details here",
-          style: TextStyle(fontSize: 16),
-          textAlign: TextAlign.center,
+    final db = Localstore.instance;
+
+    Future<Widget> searchHistoryFuture() async {
+      final items = await db.collection('search_history').get();
+      List<Movie> movieHistory = [];
+      if (items != null) {
+        items.forEach((key, value) {
+          movieHistory.add(Movie.fromJson(value));
+        });
+      }
+      List<Widget> historyMovieTiles = [];
+      for (int x = movieHistory.length - 1; x >= 0; x--) {
+        Movie movie = movieHistory[x];
+        historyMovieTiles.add(ListTile(
+          title: Text(movie.Title),
+          leading: CircleAvatar(backgroundImage: NetworkImage(movie.Poster)),
+          trailing: const Icon(Icons.arrow_forward),
+        ));
+      }
+
+      historyMovieTiles =
+          ListTile.divideTiles(context: context, tiles: historyMovieTiles)
+              .toList();
+
+      return Column(children: historyMovieTiles);
+    }
+
+    Widget searchHistory() {
+      return Expanded(
+        child: Column(
+          children: [
+            const Text("Search history",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            const SizedBox(height: 8),
+            FutureBuilder(
+              future: searchHistoryFuture(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: SizedBox(
+                      width: 100.0,
+                      height: 100.0,
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                } else {
+                  return snapshot.data!;
+                }
+              },
+            ),
+          ],
         ),
-        const SizedBox(height: 8),
-        TextField(
-          controller: serachBarController,
-          decoration: InputDecoration(
-            border: const OutlineInputBorder(),
-            suffixIcon: const Icon(Icons.search),
-            errorText: errorText == "" ? null : errorText,
-          ),
-        ),
-      ],
-    );
+      );
+    }
 
     final searchButton = ElevatedButton(
       onPressed: () async {
@@ -46,9 +87,13 @@ class _HomeState extends State<Home> {
             errorText = movie.Error;
           } else {
             errorText = "";
+            final id = db.collection('search_history').doc().id;
+            db.collection('search_history').doc(id).set(movie.toJson());
+
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => MovieDetails(movie: movie)),
+              MaterialPageRoute(
+                  builder: (context) => MovieDetails(movie: movie)),
             );
           }
         }
@@ -62,8 +107,9 @@ class _HomeState extends State<Home> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(children: [
-          searchBar,
-          //SearchHistory,
+          SearchBar(serachBarController: serachBarController, errorText: errorText),
+          SizedBox(height: 24),
+          searchHistory(),
           searchButton,
         ]),
       ),
---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  TextEditingController serachBarController;
+  String errorText;
+  SearchBar({Key? key, required this.serachBarController, required this.errorText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          "Place the name of the movie you want to know its details here",
+          style: TextStyle(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 8),
+        TextField(
+          controller: serachBarController,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            suffixIcon: const Icon(Icons.search),
+            errorText: errorText == "" ? null : errorText,
+          ),
+        ),
+      ],
+    );
+  }
+}
